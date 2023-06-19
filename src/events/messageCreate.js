cce946a789c2db3dfd21bba0b1a30718b33f283b
Aki <96163662+Akio6265@users.noreply.github.com>
2023-06-20 00:34:04 +0530

    same file :wiow:

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -13,25 +13,31 @@ module.exports = {
         const command = args.shift();
         //command= 0th index element, in above case "array";
         switch (command) {
-            case 'rui':
-            case 'akarui':
-            case 'ali': try {
-                message.channel.send('**u hear a sound from no where saying:** \n **even if im not here,  fireworks gonna light ur path**\n ||<@1043111564582060052>||')
-            } catch (err) {
-                console.log(err)
-            }
-                break;
             case 'hey':
-                message.reply('Heya')
+                message.reply('Heya');
                 break;
             case 'simp':
-                message.reply('I know you simp for me :3')
+                message.reply('I know you simp for me :3');
+                break;
+            // case 'summon protham':
+            //     message.channel.send('KEEP UR EYES PEELED,THE ATMOSPHERE IS GONNA CHANGE,PROTHAM HAS APPEARED');
+            //     break
         }
+
         if (message.content.startsWith('_yaoi')) {
             const msg = message.content.slice(5);
             if (!msg) return;
             message.delete().catch(err => console.log(err));
             message.channel.send(msg).catch(err => console.log(err));
         }
-    },
+        else if (message.content.toLocaleLowerCase().includes("rui") || message.content.toLocaleLowerCase().includes("ali") || message.content.toLocaleLowerCase().includes("akarui")) {
+            try {
+                message.channel.send('**u hear a sound from no where saying:** \n **even if im not here,  fireworks gonna light ur path**\n ||<@1043111564582060052>||')
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        else if (message.content.toLocaleLowerCase().startsWith('summon protham'))
+            message.channel.send('KEEP UR EYES PEELED,THE ATMOSPHERE IS GONNA CHANGE,PROTHAM HAS APPEARED\n||<@535077495876091904>||');
+    }
 };
