b3057e0518aa7a79f9266d3d327f9e1730ee303e
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-06-27 21:13:00 +0530

    removeexpress

--- src/yoimiya.js
+++ src/yoimiya.js
@@ -1,12 +1,3 @@
-const express = require("express")
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hey there!'))
-
-app.listen(port, () =>
-    console.log(`Your app is listening a http://localhost:${port}`)
-);
 
 require('dotenv').config();
 const { Client, GatewayIntentBits, Events, EmbedBuilder, ActivityType, Collection, AuditLogEvent } = require('discord.js');
