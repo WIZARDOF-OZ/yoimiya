977a9264ca1b8108e5bf31c73fe41ed6fac9ca8c
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-06-30 20:32:16 +0530

    update

--- src/yoimiya.js
+++ src/yoimiya.js
@@ -1,3 +1,17 @@
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hey there!'));
+
+app.listen(port, () =>
+    console.log(`Your app is listening a http://localhost:${port}`),
+);
+
+
+
 require('dotenv').config();
 // Defining the values
 const { Client, GatewayIntentBits, Events, EmbedBuilder, Collection, Partials } = require('discord.js');
